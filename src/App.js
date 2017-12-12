776f171923785eac870f07227b0bec0d90cfe257
fulisha <fulisha@ciwong.com>
2017-12-12 15:22:51 +0800

    'learn.redux.js'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Button } from 'antd-mobile'
 
 class App extends Component {
   render() {
+    const boss = '李云龙'
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{boss}</h1>
+        <Oneclass></Oneclass>
       </div>
     );
   }
 }
 
+// import { addGun } from './index.redux.js'
+// <Button onClick={()=>store.dispath(addGun())}></Button>
+
+class Oneclass extends Component {
+  render() {
+    const boss = 'hi'
+    return (
+      <div>
+        <h2>{boss}</h2>
+      </div>
+    )
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { counter } from './learn.redux.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux'
+// import { counter } from './index.redux.js'
+
+const store = createStore(counter)
+//
+// function render() {
+//   ReactDOM.render(<App store="store"/>, document.getElementById('root'));
+// }
+// render()
+// store.subscribe(render)
+
+
+
+
+ReactDOM.render(<App store="store"/>, document.getElementById('root'));
