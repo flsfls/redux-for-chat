71d5da7fbca02b46c5efa7ef952651808fe175f0
flsfls <947127871@qq.com>
2017-12-20 23:47:31 +0800

    '配置文件'

--- server/server.js
+++ server/server.js
@@ -1,39 +1,15 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-const DB_URL = 'mongodb://localhost:27017/imooc'
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected', function() {
-    console.log("mongo connect success")
-})
-const User = mongoose.model('user', new mongoose.Schema({
-    user: {type: String, require: true},
-    age: {type: Number, require: true}
-}))
-// User.create({
-//     user: 'imooc',
-//     age: 18
-// }, function(err, doc){
-//     if (!err) {
-//         console.log(doc)
-//     } else {
-//         console.log(err)
-//     }
-// })
-//新建app
+const userRouter = require('./user')
+
 const app = express()
 
-app.get('/', function(req, res) {
-    res.send('<h1>Hello World</h1>')
-})
+app.use(cookieParser())
+app.use(bodyParser.json())
 
-app.get('/data', function(req, res) {
-    User.find({}, function(err, doc) {
-        res.json(doc)
-    })
-    // res.json({name: 'imooc', type: 'ITt'})
-})
-
-app.listen(9093, function() {
-    console.log("Node App at localhost: 9093")
+app.use('/user', userRouter)
+app.listen(9093, function(){
+  console.log('Node app start at port 9093')
 })
