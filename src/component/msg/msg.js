38fa9edbc4701110897da61b498d7237cd96ea23
fulisha <fulisha@ciwong.com>
2017-12-29 09:42:10 +0800

    stash

---
+++ src/component/msg/msg.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+@connect(state=>state)
+
+class Msg extends React.Component {
+  constructor() {
+
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
